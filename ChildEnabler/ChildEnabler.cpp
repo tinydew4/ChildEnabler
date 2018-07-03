9018aba11bedbc94bd813976a554d11bff1020ae
tinydew4 <tinydew4@gmail.com>
2018-07-03 15:08:16 +0900

    Add code to hide the console window before execution

--- ChildEnabler/ChildEnabler.cpp
+++ ChildEnabler/ChildEnabler.cpp
@@ -5,6 +5,7 @@
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+    for (::ShowWindow(::GetConsoleWindow(), SW_HIDE); ::IsWindowVisible(::GetConsoleWindow()); ::Sleep(1));
     for (HWND hChild = nullptr, hParent = []{ POINT CursorPos; return GetCursorPos(&CursorPos) ? WindowFromPoint(CursorPos) : nullptr; }();
         hChild = FindWindowExW(hParent, hChild, nullptr, nullptr);
         SetWindowLong(hChild, GWL_STYLE, GetWindowLong(hChild, GWL_STYLE) & ~WS_DISABLED | WS_TABSTOP));
